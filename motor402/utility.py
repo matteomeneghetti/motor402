397a7d1578d0ca61f0d8626008e249eea308c377
Matteo Meneghetti <matteo@meneghetti.dev>
2022-05-05 17:09:49 +0200

    Cast to int in packing utility methods

--- motor402/utility.py
+++ motor402/utility.py
@@ -1,19 +1,19 @@
 import struct
 
 def uint8(value):
-    return struct.pack("<B", value)
+    return struct.pack("<B", int(value))
 
 def int8(value):
-    return struct.pack("<b", value)
+    return struct.pack("<b", int(value))
 
 def uint16(value):
-    return struct.pack("<H", value)
+    return struct.pack("<H", int(value))
 
 def int16(value):
-    return struct.pack("<h", value)
+    return struct.pack("<h", int(value))
 
 def uint32(value):
-    return struct.pack("<I", value)
+    return struct.pack("<I", int(value))
 
 def int32(value):
-    return struct.pack("<i", value)
+    return struct.pack("<i", int(value))
